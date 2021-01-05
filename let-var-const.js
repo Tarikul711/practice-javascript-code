c5950b7dbb70d35c0a1c82cf8f6c3e531e3fc8da
tarikul711 <tarikul711@gmail.com>
2021-01-05 23:01:09 +0600

    Work with let , var , const keyword

--- let-var-const.js
+++ let-var-const.js
@@ -1,8 +1,53 @@
-var stName = "Outside Function"
+// 'use strict'
+// we can access and modify the outher scope var variable 
+// var stName = "Outside Function"
 
-function printNamr() {
-    stName = "Inside Function"
-    console.log(stName)
-}
-printNamr()
-console.log(stName)
+// function printNamr() {
+//     stName = "Inside Function"
+//     console.log(stName)
+// }
+// printNamr()
+// console.log(stName)
+
+
+
+// play with inside var variable.......
+
+
+// function printName() {
+//     var stName = "Inside Function"
+//     console.log(stName)
+// }
+// printName()
+// console.log(stName)
+
+// stName = "(OutSide)Not assign the variable type"
+
+// function printName() {
+//     stName = "(Inside)Not assign the variable type"
+//     console.log(stName)
+// }
+// printName()
+// console.log(stName)
+
+
+// work on let keyword in javascript ................
+
+
+// let msg = "Main Scope let keyword"
+
+// function printMsg() {
+//     let msg = "Function Scope let keyword"
+//     console.log(msg)
+// }
+// console.log(msg)
+// printMsg()
+
+
+// const msg = "Const Msg"
+
+// function printMsg() {
+//     const msg = "Changes Value"
+//     console.log(msg)
+// }
+// printMsg()
