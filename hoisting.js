2887aca0ee508076755cf524ab931cc0c37194c8
tarikul711 <tarikul711@gmail.com>
2021-01-07 10:09:22 +0600

    hosting

---
+++ hoisting.js
@@ -0,0 +1,58 @@
+// console.log(a)
+// var a = "Bangladesh"
+
+
+
+// console.log(a)
+// let a = "Bangladesh"
+
+
+
+// var LANGUAGE = "JAVA"
+// var language = "Javascript"
+// var Language = "Python"
+// console.log(LANGUAGE, language, Language)
+
+
+// let Language = "Java"
+// let language = "JAva"
+// let LANGUAGE = "Python"
+// if (Language === language) {
+//     console.log("OK")
+// }
+// console.log(Language, language, LANGUAGE)
+
+
+
+// var LANGUAGE = "Java"
+// var language = "Javascript"
+
+// function getLanguage() {
+//     if (!language) {
+//         var language = LANGUAGE
+//     }
+//     return language
+// }
+
+// console.log(getLanguage())
+
+
+
+// myFunction()
+
+// function myFunction() {
+//     language = "Javascript"
+//     var language
+//     console.log(language)
+// }
+
+
+
+
+
+// myFunc = function() {
+//     var language = "Javascript"
+//     console.log(language)
+// }
+
+// myFunc
